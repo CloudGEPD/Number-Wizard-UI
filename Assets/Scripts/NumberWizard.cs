0e5dbd6c8378263830392a1a3ce3c370f5a5cb66
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-07-17 16:31:13 -0500

    Added more functionality
    Text and number wizard guessing

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+   [SerializeField] int max;
+   [SerializeField] int min;
+   [SerializeField] TextMeshProUGUI guessText;
+
+
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+        
+    }
+
+    
+    void StartGame()
+    {
+        max += max;
+        guess = (max + min) / 2;
+        guessText.text = guessText.ToString();
+    }
+
+    void OnKeyPressUp()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+
+    }
+
+    void OnKeyPressDown()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
+        
+	
